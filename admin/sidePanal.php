bff2f136baa1057c929d0cfd6edaae14d21d76b9
TahanyEmad <tahanyemad30@gmail.com>
2025-03-22 17:14:53 +0200

    style

--- admin/sidePanal.php
+++ admin/sidePanal.php
@@ -4,7 +4,7 @@
         <li><a href="dashboard.php">🏨 Dashboard</a></li>
         <li><a href="manageUsers.php">👥 Manage Users</a></li>
         <li><a href="manageCourses.php">📚 Manage Courses</a></li>
-        <li><a href="houses.php" class="dashboard-item">🏠Houses Leaderboard</a></li>
+        <li><a href="leaderBoard.php" class="dashboard-item">🏠Houses Leaderboard</a></li>
         <li><a href="logout.php" class="logout">🚪 Logout</a></li>
     </ul>
 </div>
