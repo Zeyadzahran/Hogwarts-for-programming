554cbaee94d97bf10736873c5419d67080da93f6
shams <shamsalalfy2005@gmail.com>
2025-03-24 07:57:43 +0200

    Zeyad Courses

--- user/sidePanal.php
+++ user/sidePanal.php
@@ -3,9 +3,9 @@
     <ul>
         <li><a href="Profile.php">👤 My Profile</a></li>
         <li><a href="courses.php">📚 My Courses</a></li>
-        <li><a href="quiz.php">َ📊 Quiz</a></li>
+        <li><a href="ManageCourses.php">📚 Assign Courses</a></li>
+        <li><a href="inventory.php">🎒 Inventory</a></li>
         <li><a href="shop/shop.php">🛒 Shop</a></li>
-        <li><a href="inventory.php">🛒 Inventory</a></li>
         <li><a href="LeaderBoard.php" class="dashboard-item">🏠 Houses Leaderboard</a></li>
         <li><a href="../src/logout.php" class="logout">🚪 Logout</a></li>
     </ul>
--- user/userClass.php
+++ user/userClass.php
@@ -90,6 +90,43 @@ class user extends Dbh{
 
     }
 
+    public function getAllCourse($id)
+    {
+
+        $query ="SELECT 
+        Course.name AS course_name,  
+        Course.id AS course_id,  
+                Professor.name AS professor_name
+            FROM Course
+                JOIN User AS Professor ON Course.professor_id = Professor.id
+                    WHERE Course.id NOT IN (
+                                    SELECT course_id 
+                                    FROM Enrollment 
+                                    WHERE student_id = ?);";
+        $stmt = $this->connect()->prepare($query);
+
+         if (!$stmt->execute([$id])) {
+            $stmt = null;
+            header("location: dashboard.php?error=statementfailed");
+            exit();
+        }
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function addNewCourse($studentId, $courseId)
+    {
+        $query = "INSERT INTO Enrollment (student_id, course_id, degree) VALUES (?, ?, ?);";
+        $stmt = $this->connect()->prepare($query);
+
+        if (!$stmt->execute([$studentId, $courseId, 0])) {
+            $stmt = null;
+            header("location: dashboard.php?error=statementfailed");
+            exit();
+        }
+
+        return true; 
+    }
+
     public function havequiz($courseid){
 
         $query = "SELECT havequiz FROM Course WHERE id = :courseid";
